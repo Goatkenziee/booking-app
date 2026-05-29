193faf4216972767c91b254b30724e6ce67406ad
Goatkenziee <mackquisition@gmail.com>
2026-05-28 23:08:02 -0400

    Fix: Corrected next.config.mjs for build

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    // Fixes npm versions of date-fns
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'date-fns': require.resolve('date-fns'),
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
